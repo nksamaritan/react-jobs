afb469eba4f375567726042425eb5905a88d6de1
Nehal Kapadiya <nehal.kapadia@mobiosolutions.com>
2024-09-12 10:14:35 +0530

    job page components design

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards";
+import JobListings from "./components/JobListings";
+import ViewAllJobs from "./components/ViewAllJobs";
+const App = () => {
+  return (
+    <>
+      <Navbar />
+
+      <Hero
+        title={"Become a React Dev"}
+        subtitle={"Find the React job that fits your skills and needs"}
+      />
+
+      <HomeCards />
+
+      <JobListings />
+
+      <ViewAllJobs />
+    </>
+  );
+};
+
+export default App;
